19ffbab00ab8721eaadd5edb3731a15dd70d6d8b
ERIKO_KIJAY <32596264+Erikokijay@users.noreply.github.com>
2025-09-19 01:00:31 +0300

    added simple schemas

--- consts.go
+++ consts.go
@@ -5,3 +5,30 @@ type SimpleResponse struct {
 	Error   string `json:"error,omitempty"`
 	Message string `json:"message,omitempty"`
 }
+
+type SearchLimit struct {
+	Limit  int    `json:"limit" form:"limit"`
+	Offset int    `json:"offset" form:"offset"`
+	Search string `json:"search" form:"search"`
+}
+
+type SimpleLimit struct {
+	Limit  int `json:"limit" form:"limit"`
+	Offset int `json:"offset" form:"offset"`
+}
+
+type SimpleId struct {
+	Id int `json:"id" form:"id"`
+}
+
+func Error(err error) SimpleResponse {
+	return SimpleResponse{Ok: false, Error: err.Error()}
+}
+
+func Success(msg string) SimpleResponse {
+	return SimpleResponse{Ok: true, Message: msg}
+}
+
+func Ok() SimpleResponse {
+	return SimpleResponse{Ok: true}
+}
